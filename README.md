4e37a5e38bc26d04dcea69f90d187d904e648ab9
Marco Aurélio Dilda Campos <madcampos@outlook.com>
2016-02-19 03:48:30 -0300

    Main functionality added.

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
-# tree-exec
-Node.js cli utility that walks a given path and executes a command for each file.
+# Tree exec
+Node.js cli utility that walks a path and executes a command for each file on the path.
+
+## Motivation
+Trying to use NPM as a build tool can be complicated in windows, the biggest problem beeing the shell don't expand glob patterns, so some utilities need a for loop to work properly, to aid with interop this package was developed.
+
+## Basic usage
+```
+$ tree-exec [options] <command>
+```
+
+## Examples:
+```
+$ tree-exec -a command @f
+$ tree-exec -g="./**/*.ext" command @f
+$ tree-exec -r="[^node_modules]\/.*$" command @f
+```
+
+## Options:
+| Parameter: | Description: |
+| :---: | :--- |
+| `-h`, `--help` | Show this information. |
+| `-p`, `--path` | The base path to start the search, if it is different from the current working directory. |
+| `-d`, `--depth` | Sets the depth to search. |
+| `-g`, `--glob` | A glob pattern to use as search. |
+| `-r`, `--regex` | A RegExp pattern to use as search. |
+| `-a`, `--all` | Search throug all files. |
+| `-i`, `--icase` | Set case insensitiveness of the search. |
+| `-x`, `--dot` | Sets if it should include paths starting with `.`. |
+| `-l`, `--library` | Sets if it should include library paths like `node_modules` or `bower_components`. |
+| `-u`, `--unicode` | Set the unicode flag. **NOTE:** Only use if your node version supports it, or it will give wrong results. |
+| `-s`, `--globstar` | Disable `**` matching against multiple folder names. |
+| `-c`, `--nocoment` | Suppress the behavior of treating `#` at the start of a pattern as a comment. |
+| `-n`, `--nonegate` | Suppress the behavior of treating a leading `!` character as negation. |
+| `-e`, `--noext` | Disable "extglob" style patterns like `+(a¦b)`. |
+| `-b`, `--nobrace` | Do not expand `{a,b}` and `{1..3}` brace sets. |
+| `-m`, `--mathbase` | If set, then patterns without slashes will be matched against the basename of the path if it contains slashes. For example, `a?b` would match the path `/xyz/123/acb`, but not `/xyz/acb/123`. |
+
+## Replace options
+The following options can be used after `@`:
+
+| Option | Result | Example |
+| :---: | :--- | :--- |
+| `f` | The path, name and extension of the file. | `./dir/file.ext` |
+| `n` | The name of the file. | `file` |
+| `e` | The extension of the file (with dot). | `.ext` |
+| `r` | The relative path of the file. | `./dir/` |
+| `p` | The full path of the file. | `/home/user/dir/` |
+| `s` | The file size (in human redable format). | `1KB` |
+| `z` | The file size (in bytes). | `1024` |
+| `m` | The file's last modified timestamp. | `2016116043022876` |
+| `t` | The file's creation timestamp. | `2016116043022876` |
+
+If the options are preceeded by a `~` then the path will be unquoted.
+Eg.: `@~f` will produce `./dir/file.ext`
+
+## TODO
+- Test against git bash and other *nix shells for completeness.
+- Add attributes flag to file.
---
+++ index.js
@@ -0,0 +1,173 @@
+/*global process*/
+'use strict';
+let fs = require('fs');
+let path = require('path');
+let minimatch = require('minimatch');
+
+/**
+ * Filters a path based on a regexp or glob pattern.
+ * @param {String} basePath The base path where the search will be performed.
+ * @param {String} pattern A string defining a regexp of a glob pattern.
+ * @param {String} type The search pattern type, can be a regexp or a glob.
+ * @param {Object} options A object containing options to the search.
+ * @param {Object} options.globOptions The options passed to minimatch's glob parser.
+ * @param {String} options.flags A string containig the flags that will be passed to the regexp pattern.
+ * @param {Number} options.depth The maximum directory depth to do the search.
+ * @param {Boolean} options.excludeDot A option to set if files and directories starting with `.` will be excluded from the search.
+ * @return {Array} A list containing the filtered paths.
+ * @see https://github.com/isaacs/minimatch
+ */
+function pathFilter(basePath, pattern, type, options){
+	let fileList = [];
+	let searchPattern;
+	let maxDepth = options.depth || 10;
+
+	/**
+	 * Walks a directory
+	 * @param {String} dir The directory path to search.
+	 * @param {Number} currentDepth The current directory depth.
+	 */
+	function walk(dir, currentDepth){
+		fs.readdirSync(dir).forEach((file) => {
+			let fullPath = path.join(dir, file);
+			if (fs.lstatSync(fullPath).isDirectory()) {
+				if (currentDepth < maxDepth) {
+					walk(path.join(fullPath), currentDepth + 1);
+				}
+			} else {
+				fileList.push(fullPath);
+			}
+		});
+	}
+
+	if (!fs.lstatSync(basePath).isDirectory()) {
+		throw new Error('Path is not a directory.');
+	}
+
+	walk(basePath, 0);
+
+	if (type === 'glob') {
+		return fileList.filter(minimatch.filter(pattern, options.globOptions));
+	}
+
+	if (options.excludeDot) {
+		let excludeDotRegexp = /\\\.|\/\./;
+		fileList = fileList.filter((file) => !excludeDotRegexp.test(file));
+	}
+
+	if (options.excludeLibrary) {
+		let excludeNodeModules = /node_modules|jspm_packages|bower_components/;
+		fileList = fileList.filter((file) => !excludeNodeModules.test(file));
+	}
+
+	if (type === 'all') {
+		searchPattern = new RegExp('^.*$', options.flags);
+	}
+
+	if (type === 'regexp') {
+		searchPattern = new RegExp(pattern, options.flags);
+	}
+
+	return fileList.filter((file) => searchPattern.test(file));
+}
+/**
+ * Path transformation function based on given string parameters.
+ * @param {String} transformString The transformation string.
+ * @returns {Function} A function that accepts a path as argument and return it's transformation.
+ */
+function transformPath(transformString){
+	let flags = transformString.match(/[~pnerfszmt]/gi);
+
+	if (!flags) {
+		throw new Error('No transformation pattern.');
+	}
+
+	/**
+	 * Formats a given date.
+	 * @param {Date} date The date to format.
+	 * @return {String} A string containing the formated date.
+	 */
+	function dateFormat(date){
+		return `${date.getFullYear()}${('00' + date.getMonth()).slice(-2)}${('00' + date.getDate()).slice(-2)}${('00' + date.getHours()).slice(-2)}${('00' + date.getMinutes()).slice(-2)}${('00' + date.getSeconds()).slice(-2)}${('000' + date.getMilliseconds()).slice(-3)}`;
+	}
+
+	/**
+	 * Formats a size to human redable numbers.
+	 * @param {Number} size The size in bytes.
+	 * @returns {String} The size in human redable format.
+	 */
+	function sizeFormat(size){
+		//TODO: refatcor the method, see if there is a better alternative
+		let i = -1;
+		let byteUnits = ['kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+		let bytes = size;
+
+		do {
+			bytes = bytes / 1024;
+			i++;
+		} while (bytes > 1024);
+
+		return Math.max(bytes, 0.1).toFixed(1) + byteUnits[i];
+	}
+
+	//Optimize for the most common case
+	if (flags.indexOf('f') !== -1 && flags.length === 1) {
+		return (file) => file;
+	}
+
+	return function(file){
+		let status = fs.lstatSync(file);
+		let filePath = '';
+
+		if (flags.indexOf('a') !== -1) {
+			//TODO: add attributes flag
+		}
+
+		if (flags.indexOf('m') !== -1) {
+			filePath += dateFormat(status.ctime);
+		}
+
+		if (flags.indexOf('t') !== -1) {
+			filePath += dateFormat(status.birthtime);
+		}
+
+		if (flags.indexOf('z') !== -1) {
+			filePath += status.size;
+		}
+
+		if (flags.indexOf('s') !== -1) {
+			filePath += sizeFormat(status.size);
+		}
+
+		if (flags.indexOf('r') !== -1 && flags.indexOf('p') === -1 && flags.indexOf('f') === -1) {
+			filePath += path.dirname(path.relative(process.cwd(), file)) + path.sep;
+		}
+
+		if (flags.indexOf('p') !== -1 && flags.indexOf('f') === -1) {
+			filePath += path.dirname(path.resolve(file)) + path.sep;
+		}
+
+		if (flags.indexOf('f') !== -1) {
+			filePath += path.resolve(file);
+		}
+
+		if (flags.indexOf('n') !== -1) {
+			filePath += path.basename(file, path.extname(file));
+		}
+
+		if (flags.indexOf('e') !== -1) {
+			filePath += path.extname(file);
+		}
+
+		if (flags.indexOF('~') !== -1) {
+			filePath = `"${filePath}"`;
+		}
+
+		return filePath;
+	};
+}
+
+module.exports = {
+	filter: pathFilter,
+	transform: transformPath
+};
